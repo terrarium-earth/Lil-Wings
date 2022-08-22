9bd2ad62cb75f0c9f1a424a252b083ac10c4ca65
CodexAdrian <83074853+CodexAdrian@users.noreply.github.com>
2022-08-21 20:45:41 -0400

    version bumpo

--- common/src/main/java/com/toadstoolstudios/lilwings/registry/LilWingsBlocks.java
+++ common/src/main/java/com/toadstoolstudios/lilwings/registry/LilWingsBlocks.java
@@ -15,7 +15,6 @@ import net.minecraft.sound.BlockSoundGroup;
 import java.util.function.Supplier;
 
 public class LilWingsBlocks {
-    
 
     public static final Supplier<Block> BUTTERFLY_JAR = CommonServices.REGISTRY.registerBlock("butterfly_jar", ButterflyJarBlock::new);
     //public static final RegistryObject<Item> BUTTERFLY_JAR_ITEM = CommonServices.REGISTRY.registerItem("butterfly_jar", () -> new ButterflyJarItem(BUTTERFLY_JAR.get()));
@@ -24,7 +23,7 @@ public class LilWingsBlocks {
     public static final Supplier<Block> BUTTERNITE_BLOCK = CommonServices.REGISTRY.registerBlock("butternite_block", () -> new Block(AbstractBlock.Settings.of(Material.METAL).requiresTool().strength(4.0f)));
     public static final Supplier<Item> BUTTERNITE_BLOCK_ITEM = CommonServices.REGISTRY.registerItem("butternite_block", () -> new BlockItem(BUTTERNITE_BLOCK.get(), new Item.Settings().group(LilWings.TAB)));
 
-    public static final Supplier<Block> BUTTER_BLOCK = CommonServices.REGISTRY.registerBlock("butter_block", () -> new Block(AbstractBlock.Settings.of(Material.SOLID_ORGANIC).sounds(BlockSoundGroup.HONEY).strength(0.5f)));
+    public static final Supplier<Block> BUTTER_BLOCK = CommonServices.REGISTRY.registerBlock("butter_block", () -> new Block(AbstractBlock.Settings.of(Material.SOLID_ORGANIC).sounds(BlockSoundGroup.HONEY).strength(0.5f).slipperiness(.98f)));
     public static final Supplier<Item> BUTTER_BLOCK_ITEM = CommonServices.REGISTRY.registerItem("butter_block", () -> new BlockItem(BUTTER_BLOCK.get(), new Item.Settings().group(LilWings.TAB)));
     //TODO make butter slippery
 
