b34d820f6311bb7196f63bceff7a9754135e1380
Daniel Jagt <djjagt@ncsu.edu>
2022-11-04 21:59:37 -0400

    [Implementation] Fixed Forge milk to butter implementation and added milk cauldron tag

--- common/src/main/java/com/toadstoolstudios/lilwings/block/jareffects/ButterGoldJarEffect.java
+++ common/src/main/java/com/toadstoolstudios/lilwings/block/jareffects/ButterGoldJarEffect.java
@@ -5,6 +5,7 @@ import com.toadstoolstudios.lilwings.registry.LilWingsBlocks;
 import dev.architectury.injectables.annotations.ExpectPlatform;
 import net.minecraft.block.Block;
 import net.minecraft.particle.ParticleEffect;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class ButterGoldJarEffect implements JarEffect {
@@ -12,16 +13,15 @@ public class ButterGoldJarEffect implements JarEffect {
     @Override
     public void tickEffect(World level, ButterflyJarBlockEntity blockEntity) {
         if(level.isClient) return;
-        if(level.getBlockState(blockEntity.getPos().down()).isOf(getMilkCauldron())) {
+        if(isMilkCauldron(level, blockEntity.getPos().down())) {
             if ((int)(Math.random() * 1000) == 1) {
                 level.setBlockState(blockEntity.getPos().down(), LilWingsBlocks.BUTTER_CAULDRON.get().getDefaultState(), 2);
             }
         }
-
     }
 
     @ExpectPlatform
-    public static Block getMilkCauldron() {
+    public static boolean isMilkCauldron(World level, BlockPos bPos) {
         throw new AssertionError();
     }
 
--- forge/src/main/java/com/toadstoolstudios/lilwings/block/jareffects/forge/ButterGoldJarEffectImpl.java
+++ forge/src/main/java/com/toadstoolstudios/lilwings/block/jareffects/forge/ButterGoldJarEffectImpl.java
@@ -2,9 +2,17 @@ package com.toadstoolstudios.lilwings.block.jareffects.forge;
 
 import com.toadstoolstudios.lilwings.forge.ForgeLilWings;
 import net.minecraft.block.Block;
+import net.minecraft.tag.BlockTags;
+import net.minecraft.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class ButterGoldJarEffectImpl {
-    public static Block getMilkCauldron() {
-        return ForgeLilWings.MILK_CAULDRON.get();
+
+    public static final TagKey<Block> CAULDRON_TAG = BlockTags.create(new Identifier("forge", "milk_cauldron"));
+
+    public static boolean isMilkCauldron(World level, BlockPos bPos) {
+        return level.getBlockState(bPos).isIn(CAULDRON_TAG);
     }
 }
