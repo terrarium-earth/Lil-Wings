d43f56c589543998965d18b168589d6ff6391d14
CodexAdrian <83074853+CodexAdrian@users.noreply.github.com>
2022-11-21 16:35:25 -0500

    Im sorry JaidenAnimations

--- common/src/main/java/com/toadstoolstudios/lilwings/client/patron/PatreonFluttererRenderer.java
+++ common/src/main/java/com/toadstoolstudios/lilwings/client/patron/PatreonFluttererRenderer.java
@@ -1,10 +1,22 @@
 package com.toadstoolstudios.lilwings.client.patron;
 
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.util.Identifier;
 import software.bernie.geckolib3.model.provider.GeoModelProvider;
 import software.bernie.geckolib3.renderers.geo.IGeoRenderer;
 
 public class PatreonFluttererRenderer implements IGeoRenderer<PatreonFlutteringButterfly> {
+    VertexConsumerProvider vertexConsumerProvider;
+
+    @Override
+    public void setCurrentRTB(VertexConsumerProvider rtb) {
+        this.vertexConsumerProvider = rtb;
+    }
+
+    @Override
+    public VertexConsumerProvider getCurrentRTB() {
+        return vertexConsumerProvider;
+    }
 
     @Override
     public PatreonFlutteringModel getGeoModelProvider() {
